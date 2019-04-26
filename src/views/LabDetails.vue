1c608b0802c8d9ad9d05c65daf8fc378382d54e9
Christian Caicedo <ccaicedo@credera.com>
2019-04-25 20:31:31 -0500

    Started working on the lab page adding content. Just header for now.

--- src/views/LabDetails.vue
+++ src/views/LabDetails.vue
@@ -1,14 +1,34 @@
 <template>
-  <v-layout>
-   <!-- <v-flex xs12>
-     <v-layout row wrap>
-       <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
-     </v-layout>
-   </v-flex> -->
-   <v-layout>
-      <v-flex xs-12 class="display-3">
-          {{title}} {{ $route.params.id }}
+  <v-layout
+      class="fill-height"
+      tag="v-list"
+      column
+  >
+    <v-list>
+      <v-list-tile avatar>
+        <v-list-tile-title class="title">
+          <router-link to="/">
+            <v-icon>arrow_back</v-icon>    
+            Return
+          </router-link>
+          
+        </v-list-tile-title>
+      </v-list-tile>
+      <v-divider/>
+    </v-list>
+
+    <v-layout>
+      
+      <v-flex xs12>
+        <v-layout row wrap>
+          <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
+        </v-layout>
       </v-flex>
+      <v-layout>
+        <v-flex xs-12 class="display-3">
+            {{title}} {{$route.params.id}}
+        </v-flex>
+      </v-layout>
     </v-layout>
   </v-layout>
 </template>
