57dd7431e8e1156325c8924d53ba02428e23b1b4
Christian Caicedo <ccaicedo@credera.com>
2019-04-25 22:39:09 -0500

    added css, a markdown file and some changes to the lab details file.

--- src/views/LabDetails.vue
+++ src/views/LabDetails.vue
@@ -1,35 +1,28 @@
+<style src="../assets/css/LabDetails.css" scoped>
+ 
+</style>
 <template>
-  <v-layout
+    <v-layout
       class="fill-height"
       tag="v-list"
       column
-  >
-    <v-list>
-      <v-list-tile avatar>
-        <v-list-tile-title class="title">
-          <router-link to="/">
-            <v-icon>arrow_back</v-icon>    
-            Return
-          </router-link>
-          
-        </v-list-tile-title>
-      </v-list-tile>
-      <v-divider/>
-    </v-list>
-
-    <v-layout>
-      
-      <v-flex xs12>
-        <v-layout row wrap>
-          <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
-        </v-layout>
-      </v-flex>
+    >
+      <v-list>
+        <v-list-tile avatar>
+          <v-list-tile-title class="title">
+            <router-link to="/" v-bind:class="{ returnLink: true}">
+              <v-icon>arrow_back</v-icon>    
+              Return
+            </router-link>
+          </v-list-tile-title>
+        </v-list-tile>
+        <v-divider/>
+      </v-list>
       <v-layout>
         <v-flex xs-12 class="display-3">
             {{title}} {{$route.params.id}}
         </v-flex>
       </v-layout>
-    </v-layout>
   </v-layout>
 </template>
 
