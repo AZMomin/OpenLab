5ba3f7fa8f6d6bac153431a7887d04e5c24f84ec
amomin <amomin>
2019-04-28 18:57:11 -0500

    added LabDetails page and refactored data to JSON files

--- src/views/LabDetails.vue
+++ src/views/LabDetails.vue
@@ -1,37 +1,65 @@
 <template>
-  <v-layout>
-   <!-- <v-flex xs12>
-     <v-layout row wrap>
-       <v-breadcrumbs :items="items" divider=">"></v-breadcrumbs>
-     </v-layout>
-   </v-flex> -->
-   <v-layout>
-      <v-flex xs-12 class="display-3">
-          {{title}} {{ $route.params.id }}
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs8>
+        <div xs12 class="display-3"> {{ lab.title }} </div>
+        <div xs12 class="headline"> {{ lab.description }} </div>
+      </v-flex>
+      <v-flex xs4>
+        <v-img
+          :src=lab.img
+          height="150px"
+          contain
+        ></v-img>
       </v-flex>
     </v-layout>
-  </v-layout>
+    <v-divider class="mt-4"/>
+    <v-layout row>
+      <v-flex xs9 class="border-divider" mr-3>
+        <div xs12 class="display-1"> Placeholder </div>
+      </v-flex>
+      <v-flex xs3>
+        <div xs12 class="headline">Technologies</div>
+        <v-list-tile
+          v-for="tech in lab.technologies"
+          :key="tech.title"
+        >
+          <v-list-tile-action>
+            <v-avatar
+                size="40px"
+              >
+                <img
+                  :src="tech.img"
+                >
+              </v-avatar>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ tech.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import labs from '../data/labs.json'
+import _ from 'lodash'
 
 export default {
   components: {
   },
+  created: function () {
+    this.lab = _.find(labs, lab => lab.id === parseInt(this.$route.params.id))
+  },
   data: () => ({
-    title: 'Lab Title',
-    items: [
-      {
-        text: 'Labs',
-        disabled: false,
-        href: '#/labs'
-      },
-      {
-        text: 'Lab Title',
-        disabled: true,
-        href: 'breadcrumbs_link_2'
-      }
-    ]
+    lab: {}
   })
 }
 </script>
+
+<style>
+.border-divider {
+  border-right: solid 1px lightgray;
+}
+</style>
